7e4ac7f5366c0f025679c5f89003259b9a3f3d4f
RavDas <86109995+RavDas@users.noreply.github.com>
2025-10-28 17:22:26 +0530

    Create run.py in root level

---
+++ maintainence-service/run.py
@@ -0,0 +1,21 @@
+from app import create_app, db
+import os
+
+app = create_app(os.environ.get('FLASK_ENV', 'development'))
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'MaintenanceItem': __import__('app.models.maintenance').models.maintenance.MaintenanceItem
+    }
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
